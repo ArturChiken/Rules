0cbb624c3d8f52cf4a6555dffe501f15cf814c6c
Artur Yakimenko <ponygangster@gmail.com>
2025-10-22 13:00:02 +0300

    added fent's files

--- Rules/Assets/_Scripts/Movement.cs
+++ Rules/Assets/_Scripts/Movement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff63e9e52ac4bfe3bce38891e8fac1b5175fb6a8481c567b0f938a773e0e887c
-size 988
+oid sha256:280bdb1170ad567086f2634817c641b854f70ba221b9500e40905539bdeae8de
+size 2487
