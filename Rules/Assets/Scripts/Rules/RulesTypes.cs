405025a8883372ba9248b87e79ce0321a783295d
Artur Yakimenko <ponygangster@gmail.com>
2026-04-16 15:56:04 +0300

    boo

--- Rules/Assets/Scripts/Rules/RulesTypes.cs
+++ Rules/Assets/Scripts/Rules/RulesTypes.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 
-public class RulesTypes : MonoBehaviour
+public abstract class RulesTypes : MonoBehaviour
 {
     public RulesManager rulesManager;
 
     public bool isActive;
     public bool isDone;
+    public int ruleID;
 }
